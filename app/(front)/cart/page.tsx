f54274e93ea7b8e87158ca465e8b4e01934db50f
Matiasmarensi <96029066+Matiasmarensi@users.noreply.github.com>
2024-04-22 21:25:20 -0300

    cartDetails

---
+++ app/(front)/cart/page.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import CartDetails from "./CartDetail";
+
+const CartPage = () => {
+  return (
+    <div>
+      <CartDetails />
+    </div>
+  );
+};
+
+export default CartPage;
