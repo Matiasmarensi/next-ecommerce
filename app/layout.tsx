78dda01c609e7ee6870ed1cebd866f292d10c764
Matiasmarensi <96029066+Matiasmarensi@users.noreply.github.com>
2024-04-23 20:02:30 -0300

    providers, credentials

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import Header from "../components/header/header";
+import Providers from "@/components/Providers";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -18,13 +19,15 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
-        <div className="min-h-screen flex flex-col">
-          <Header />
-          <main className="flex-grow container mx-auto px-4">{children}</main>
-          <footer className="footer  footer-center p-4 bg-base-400 text-base-content ">
-            <p>Copyright &copy; {new Date().getFullYear()} - All right reserved</p>
-          </footer>
-        </div>
+        <Providers>
+          <div className="min-h-screen flex flex-col">
+            <Header />
+            <main className="flex-grow container mx-auto px-4">{children}</main>
+            <footer className="footer  footer-center p-4 bg-base-400 text-base-content ">
+              <p>Copyright &copy; {new Date().getFullYear()} - All right reserved</p>
+            </footer>
+          </div>
+        </Providers>
       </body>
     </html>
   );
